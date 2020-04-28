ad4e2e30b4ffb55d31b4424193e2dc6ec412550c
santuan <smrcouto88@gmail.com>
2020-04-27 21:01:11 -0300

    offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,6 +74,7 @@ module.exports = {
         theme_color: `#2d3748`,
         display: `standalone`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        cache_busting_mode: "none",
       },
     },
     {
@@ -99,6 +100,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
