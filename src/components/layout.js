6127d5e0556cff0d919b12f5c4b094471fa2a674
santuan <smrcouto88@gmail.com>
2020-09-29 14:02:26 -0300

    love and code

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,8 @@ import PropTypes from "prop-types"
 import "./layout.css"
 import tw from "tailwind.macro"
 import styled from "@emotion/styled"
-import { TiHeartFullOutline } from "react-icons/ti"
+import { AiOutlineHeart } from "react-icons/ai"
+import { HiCode } from "react-icons/hi"
 
 const Layout = ({ children }) => {
   return (
@@ -21,8 +22,10 @@ const Layout = ({ children }) => {
           © {new Date().getFullYear()}, Realizado en Gatsby + Contentful +
           Netlify{" "}
           <div className="mt-2 opacity-75">
-            Hecho con
-            <TiHeartFullOutline className="inline-block mx-1 mb-1 text-lg text-red-600" />
+            <span className="block mx-1 mb-2">Hecho con</span>
+            <HiCode className="inline-block mx-1 mb-1 text-lg text-white" />
+            <span className="mx-1">&</span>
+            <AiOutlineHeart className="inline-block mx-1 mb-1 text-lg text-white" />
           </div>
         </Footer>
       </App>
