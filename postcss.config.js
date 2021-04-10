5b5012a189f8c4e1e78328181e8ddcf5d850db15
santuan <smrcouto88@gmail.com>
2021-04-09 23:30:43 -0300

    tailwind 21

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,7 @@
+// postcss.config.js
 module.exports = {
-  plugins: [
-    require("tailwindcss")("./tailwind.config.js"),
-    require("postcss-nested"),
-    require("postcss-preset-env")({
-      features: {
-        "nesting-rules": true,
-      },
-      browsers: ["> 1%", "last 2 versions", "Firefox ESR"],
-    }),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
+  purge: false,
   theme: {
     fontFamily: {
       sans: ["Josefin Sans", "serif"],
@@ -7,7 +10,22 @@ module.exports = {
     },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    plugin(function({ addUtilities }) {
+      const newUtilities = {
+        '.kush-center': {
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+      }
+      addUtilities(newUtilities)
+    }),
+    require("postcss-nested"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
+  ],
   future: {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
