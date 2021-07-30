2f8dd493ec621434971c0b16580f594f4e09f439
stn <smrcouto88@gmail.com>
2021-07-30 16:47:58 -0300

    text loop

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 import { navigate } from "gatsby"
 import { AwesomeButton } from "react-awesome-button"
 import Fade from "react-reveal/Fade"
@@ -6,6 +7,7 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import "../styles/bgPattern.css"
 import Confetti from "react-dom-confetti"
+import TextLoop from "react-text-loop"
 
 const copyToClipboard = (str) => {
   const el = document.createElement("textarea")
@@ -41,10 +43,10 @@ export default function IndexPage() {
       <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden text-center bg-gray-900 shadow-sm bg-pattern">
         <div className="relative z-50 w-full max-w-lg px-6 mx-auto mt-12 mb-2 text-center">
           <h2
-            className="w-full font-serif text-6xl text-white md:text-center"
+            className="w-full font-serif text-6xl text-white md:text-left"
             style={{ opacity: ".8" }}
           >
-            <span className="inline-block py-1 mr-1 text-red-500 transition-all duration-500 rounded-md hover:text-gray-900 md:px-4 hover:font- cursor-help hover:font-mono hover:bg-gray-800">
+            <span className="inline-block py-1 mr-1 font-mono text-red-500 transition-all duration-500 transform -translate-x-4 rounded-md hover:translate-x-0 hover:text-gray-900 md:px-4 hover:bg-gray-800">
               Desarrollo
             </span>
             <br />y
@@ -55,12 +57,52 @@ export default function IndexPage() {
                 setTimeout(() => setIsCopied(false), 3000)
               }}
               className={
-                "px-2 ml-1 inline-block py-1 transition-all duration-500 text-blue-500 rounded-md hover:font-serif hover:text-yellow-400 "
+                "px-2 ml-1 inline-block py-1 transition-all duration-500 text-blue-500 rounded-md cursor-help hover:underline hover:font-serif hover:text-blue-600 "
               }
             >
               Diseño
-            </button>{" "}<br />
-            cosas.
+            </button>{" "}
+            <div className="block w-full">
+              <TextLoop interval={3000} fade={true}>
+                <Link
+                  to={`/colaboraciones/germinacion-del-camino/`}
+                  className="text-purple-400 hover:text-white hover:underline"
+                >
+                  aplicaciones
+                </Link>
+                <Link
+                  to={`/colaboraciones/bomberos-voluntarios-de-lujan-de-cuyo`}
+                  className="text-yellow-400 hover:text-white hover:underline"
+                >
+                  páginas
+                </Link>
+                <Link
+                  to={`/colaboraciones/alan-sutton/`}
+                  className="text-yellow-400 hover:text-white hover:underline"
+                >
+                  webs
+                </Link>
+                <Link
+                  to={`/colaboraciones/clinicas-colectivas`}
+                  className="text-yellow-400 hover:text-white hover:underline"
+                >
+                  porfolios
+                </Link>
+
+                <Link
+                  to={`/colaboraciones/kallpa-ludica`}
+                  className="text-yellow-400 hover:text-white hover:underline"
+                >
+                  tienditas
+                </Link>
+                <Link
+                  to={`/colaboraciones/`}
+                  className="text-yellow-400 hover:text-white hover:underline"
+                >
+                  cosas
+                </Link>
+              </TextLoop>
+            </div>
           </h2>
           <div className="transform -translate-y-48 translate-x-52">
             <Confetti active={isCopied} config={config} />
