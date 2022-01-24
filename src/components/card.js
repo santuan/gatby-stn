52f6824150c6be82a448bdd42e79942e96493cac
stn <smrcouto88@gmail.com>
2022-01-24 11:05:36 -0300

    card hover

--- src/components/card.js
+++ src/components/card.js
@@ -6,10 +6,10 @@ import { Link } from "gatsby"
 
 const Card = ({ card }) => (
   <div
-    className="relative top-0 flex w-full overflow-hidden duration-700 transform bg-indigo-900 rounded-md shadow-lg hover:-translate-y-2"
+    className="relative top-0 flex w-full overflow-hidden duration-700 transform bg-indigo-900 rounded-md shadow-lg group hover:scale-105"
     style={{ minHeight: "230px" }}
   >
-    <div className="relative z-10 w-full px-6 py-6 pb-4 text-left duration-700 bg-opacity-75 sm:from-indigo-900 bg-gradient-to-br hover:bg-indigo-700">
+    <div className="relative z-10 w-full px-6 py-6 pb-4 text-left duration-700 bg-opacity-75 sm:from-indigo-900 bg-gradient-to-br hover:bg-indigo-700 hover:bg-opacity-80">
       <time className="block mb-2 font-mono text-sm text-white uppercase opacity-90">
         {card.createdAt}
       </time>
@@ -37,7 +37,7 @@ const Card = ({ card }) => (
     </div>
     <Link
       to={`/blog/${card.slug}`}
-      className="absolute inset-0 z-0 w-full opacity-20 md:opacity-30 "
+      className="absolute inset-0 z-0 w-full duration-1000 opacity-20 md:opacity-40 group-hover:scale-105 "
     >
       <GatsbyImage
         title={card.title}
